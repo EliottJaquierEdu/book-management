858b13c8ffc86ce75d2ffde32ba4e8dca80a7edf
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-11-01 20:45:14 +0100

    Adding a bunch of stuffs, not a good one's too, sorry :)

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,11 +1,37 @@
-<script setup lang="ts">
-
+<script lang="ts">
+import {ref} from "vue";
 </script>
 
+
 <template>
-<input type="search" value="Livre pour les nuls">
+  <div id="search">
+    <i class="fa-solid fa-magnifying-glass"></i>
+    <input type="search" placeholder="Rechercher des livres...">
+  </div>
 </template>
 
 <style scoped>
+#search {
+  display: flex;
+  align-items: center;
+  background-color: $primary;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 100%;
+  margin: 2rem 0;
+}
 
+#search input {
+  border: none;
+  outline: none;
+  background: none;
+  width: 100%;
+  color: #333;
+  margin: 0;
+  padding: 0.5rem;
+}
+
+#search input::placeholder {
+  color: #aaa;
+}
 </style>
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,35 +1,98 @@
+<template>
+  <div id="top-bar">
+    <div @click="toggleHamMenu" :class="{ 'ham-menu__active': hamMenuOpen }">
+      <i class="fa-solid fa-bars"></i>
+    </div>
+    <div>
+      <i class="fa-solid fa-search"></i>
+      <i class="fa-solid fa-user"></i>
+    </div>
+  </div>
+  <div class="ham-menu" :class="{ 'open': hamMenuOpen }">
+    <div id="ham-menu__top-bar">
+      <div>
+        <i class="fa-solid fa-xmark" @click="closeHamMenu"></i>
+      </div>
+    </div>
+    <div id="menu-nav">
+      <subnav></subnav>
+    </div>
+  </div>
+</template>
+
 <script lang="ts">
-import {defineComponent} from 'vue'
+import { defineComponent, ref } from 'vue';
+import Subnav from "./subnav.vue";
 
 export default defineComponent({
-  name: "TopBar"
-})
-</script>
+  name: "TopBar",
+  components: { Subnav },
+  setup() {
+    const hamMenuOpen = ref(false);
 
-<template>
-<div id="top-bar">
-  <div>
-    <i class="fa-solid fa-bars"></i>
-  </div>
-  <div>
-    <i class="fa-solid fa-search"></i>
-    <i class="fa-solid fa-user"></i>
-  </div>
-</div>
-</template>
+    const toggleHamMenu = () => {
+      hamMenuOpen.value = !hamMenuOpen.value;
+    };
+
+    const closeHamMenu = () => {
+      hamMenuOpen.value = false;
+    };
+
+    return { hamMenuOpen, toggleHamMenu, closeHamMenu };
+  }
+});
+</script>
 
 <style lang="scss" scoped>
 @import "src/assets/scss/variables";
-#top-bar{
+
+#top-bar {
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 0.5rem;
   height: 3rem;
   border-bottom: 1px solid $light-dark;
-  i{
+  i {
     font-size: 1.5rem;
     margin: 0.5rem 0.5rem;
   }
+}
+
+.ham-menu {
+  z-index: 10;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 100%;
+  padding-left: 0.3rem;
+  background: white;
+  transition: width 0.3s ease;
+  display: none; /* Ajoutez cette ligne pour rendre le menu invisible par défaut */
+}
+
+.ham-menu.open {
+  width: 65%;
+  height: 100%;
+  display: block; /* Ajoutez cette ligne pour afficher le menu lorsqu'il est ouvert */
+}
+
+#ham-menu__top-bar {
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  padding: 0 0.5rem;
+  height: 3rem;
+  border-bottom: 1px solid $light-dark;
+  i {
+    font-size: 1.5rem;
+    margin: 0.5rem 0.5rem;
+  }
+}
+
+.ham-menu__active {
+  width: 10%;
+  height: 100%;
 }
 </style>
