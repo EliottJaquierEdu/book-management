1c951ef285fff6e64f3354e49fee967f19ce1bca
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-11-02 08:23:57 +0100

    optimized scss on SearchBar.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-import {ref} from "vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div id="search">
@@ -11,32 +9,33 @@ import {ref} from "vue";
 
 <style scoped lang="scss">
 @import "src/assets/scss/variables";
+
 #search {
   display: flex;
   align-items: center;
-  background-color: $lightest ;
+  background-color: $lightest;
   border: $corner-thickness solid $light-dark;
   border-radius: $corner-radius;
   width: 100%;
   margin: 2rem 0;
-}
 
-#search input {
-  border: none;
-  outline: none;
-  background: none;
-  width: 100%;
-  color: #333;
-  margin: 0;
-  padding: 0.5rem;
-}
+  input {
+    border: none;
+    outline: none;
+    background: none;
+    width: 100%;
+    color: #333;
+    margin: 0;
+    padding: 0.5rem;
+  }
 
-#search i {
-  margin: 0 1rem;
-  color: $primary;
-}
+  i {
+    margin: 0 1rem;
+    color: $primary;
+  }
 
-#search input::placeholder {
-  color: $text;
+  input::placeholder {
+    color: $text;
+  }
 }
 </style>
